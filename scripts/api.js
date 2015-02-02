c76302194137bb5c3cecc12e5a5879134a4a9125
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-02-02 12:17:35 +0000

    add a sample guardian api query

--- scripts/api.js
+++ scripts/api.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $.getJSON( 'http://content.guardianapis.com/search?api-key=test&section=uk-news', function( data ) {
     var items = [];
-    $.each( data, function( key, val ) {
-      items.push( '<li id="' + key + '">' + val + '</li>' );
+    $.each( data.response.results, function( index, val ) {
+      items.push( '<li id="' + index + '">' + val.webTitle + '</li>' );
+      console.log(data);
     });
 
     $( '<ul/>', {
