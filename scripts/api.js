1fed6cb87ccfdf7e254b16e07dff9b40a9fe8f2b
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-02-02 17:19:14 +0000

    refactor DRY

--- scripts/api.js
+++ scripts/api.js
@@ -1,46 +1,58 @@
 $(document).ready(function(){
-  $.getJSON( 'http://content.guardianapis.com/search?show-fields=trailText&q=uk-news&api-key=test', function( data ) {
-    var titles = [],
-        trail = [],
-        webUrls = [];
-    $.each( data.response.results, function( index, val ) {
-      titles.push( '<li id=title"' + index + '"><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>' );
-      console.log(data);
+
+  var searchTerms = ['uk-news','travel', 'football']
+  var request = ['http://content.guardianapis.com/search?show-fields=trailText&q=','&api-key=test']
+
+  $.each(searchTerms, function (ind, val) {
+    var thisRequest = [];
+    thisRequest.push(request[0]);
+    thisRequest.push(val);
+    thisRequest.push(request[1]);
+    thisRequest = thisRequest.join('');
+    var stories = [];
+    $.getJSON(thisRequest, function(data){
+      $.each(data.response.results, function (ind, val){
+        stories.push('<li><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>');
+      });
+    $( '<ul/>', {'class': 'titles',html: stories.join('')}).appendTo( '#'+val );
     });
-
-    $( '<ul/>', {
-      'id': 'titles',
-      html: titles.join( '' )
-    }).appendTo( '#UKNews' );
-  });
-
-  $.getJSON( 'http://content.guardianapis.com/search?show-fields=trailText&q=travel&api-key=test', function( data ) {
-    var titles = [],
-        trail = [],
-        webUrls = [];
-    $.each( data.response.results, function( index, val ) {
-      titles.push( '<li id=title"' + index + '"><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>' );
-      console.log(data);
-    });
-
-    $( '<ul/>', {
-      'id': 'titles',
-      html: titles.join( '' )
-    }).appendTo( '#travel' );
-  });
-
-  $.getJSON( 'http://content.guardianapis.com/search?show-fields=trailText&q=football&api-key=test', function( data ) {
-    var titles = [],
-        trail = [],
-        webUrls = [];
-    $.each( data.response.results, function( index, val ) {
-      titles.push( '<li id=title"' + index + '"><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>' );
-      console.log(data);
-    });
-
-    $( '<ul/>', {
-      'id': 'titles',
-      html: titles.join( '' )
-    }).appendTo( '#football' );
   });
 });
+
+//   $.getJSON( 'http://content.guardianapis.com/search?show-fields=trailText&q=uk-news&api-key=test', function( data ) {
+//     var titles1 = [];
+//     $.each( data.response.results, function( index, val ) {
+//       titles1.push( '<li id=title"' + index + '"><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>' );
+//     });
+
+//     $( '<ul/>', {
+//       'id': 'titles',
+//       html: titles1.join( '' )
+//     }).appendTo( '#UKNews' );
+//   });
+
+//   $.getJSON( 'http://content.guardianapis.com/search?show-fields=trailText&q=travel&api-key=test', function( data ) {
+//     var titles2 = [];
+//     $.each( data.response.results, function( index, val ) {
+//       titles2.push( '<li id=title"' + index + '"><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>' );
+//       console.log(data);
+//     });
+
+//     $( '<ul/>', {
+//       'id': 'titles',
+//       html: titles2.join( '' )
+//     }).appendTo( '#travel' );
+//   });
+
+//   $.getJSON( 'http://content.guardianapis.com/search?show-fields=trailText&q=football&api-key=test', function( data ) {
+//     var titles3 = [];
+//     $.each( data.response.results, function( index, val ) {
+//       titles3.push( '<li id=title"' + index + '"><a data-toggle="tooltip" title="'+ val.fields.trailText+'" href="'+ val.webUrl +'">' + val.webTitle + '</a></li>' );
+//     });
+
+//     $( '<ul/>', {
+//       'id': 'titles',
+//       html: titles3.join( '' )
+//     }).appendTo( '#football' );
+//   });
+// });
