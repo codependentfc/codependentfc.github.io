824d8601bb46f7c2684cfafeb78dda6274f68863
Jason Luu <jasoncluu@googlemail.com>
2015-02-03 15:30:53 +0000

    week2test

--- scripts/api.js
+++ scripts/api.js
@@ -30,4 +30,3 @@ $(document).ready(function() {
     });
   });
 });
-
---
+++ scripts/test.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {  
+$.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "http://content.guardianapis.com/search?api-key=test&show-fields=football",
+    success: function(data) {
+        for (var i = 0; i < data.response.results.length; i++) {
+        $("#uk-news").append(data.response.results[i].webTitle + "'>Link</a></div>");
+            }
+            console.log(data);
+        }
+    });
+ });​
