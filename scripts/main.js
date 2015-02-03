ea2a146ca7888843cb104aa8db06a7c2dbc67ef4
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-02-03 13:46:21 +0000

    fix down arrow/fixed nav conflict

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 50
         }, 1000);
         return false;
       }
