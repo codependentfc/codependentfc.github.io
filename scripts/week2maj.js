3b38b03b27030c0602c3c8c4f717b8bdd86f355a
amilvasishtha <amil_v@msn.com>
2015-02-04 22:12:35 +0000

    maj bullet points

--- scripts/week2maj.js
+++ scripts/week2maj.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
   $.getJSON("http://content.guardianapis.com/search?section=uk-news&api-key=test", function(data){
     for (i=0; i<10 ;i++) {
-  $("#uk-news").append('<ol>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</ol>')
+  $("#uk-news").append('<li>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</li>')
     console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?section=sport&api-key=test", function(data){
     for (i=0; i<10 ;i++) {
-  $("#sport").append('<ol>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</ol>')
+  $("#sport").append('<li>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</li>')
   console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?section=travel&api-key=test", function(data){
     for (i=0; i<10 ;i++) {
-  $("#travel").append('<ol>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</ol>')
+  $("#travel").append('<li>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</li>')
     console.log(data);
     }
   });
 
 });
+
+$("a.link").on("click",function(){
+              window.open('#uk-news','_blank');
+              });
