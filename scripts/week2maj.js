4d93921a2d5282d3f43c16da77991c7080bbc75e
amilvasishtha <amil_v@msn.com>
2015-02-04 16:32:59 +0000

    maj version

--- scripts/week2maj.js
+++ scripts/week2maj.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
-    $.getJSON("http://content.guardianapis.com/search?api-key=test&q=uknews", function(data){
-      for (i=0; i<10 ;i++) {
-    $("#uknews").append('<li>'+ data.response.results[i].sectionName +': ' + '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</li>')
-      console.log(data);
+  $.getJSON("http://content.guardianapis.com/search?section=uk-news&api-key=test", function(data){
+    for (i=0; i<10 ;i++) {
+  $("#uk-news").append('<ol>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</ol>')
+    console.log(data);
     }
-    });
-    $.getJSON("http://content.guardianapis.com/search?api-key=test&q=sport", function(data){
-      for (i=0; i<10 ;i++) {
-    $("#sport").append('<li>'+ data.response.results[i].sectionName +': ' + '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</li>')
-      console.log(data);
+  });
+
+$.getJSON("http://content.guardianapis.com/search?section=sport&api-key=test", function(data){
+    for (i=0; i<10 ;i++) {
+  $("#sport").append('<ol>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</ol>')
+  console.log(data);
     }
-    });
-    $.getJSON("http://content.guardianapis.com/search?q=travel&api-key=test", function(data){
-      for (i=0; i<10 ;i++) {
-    $("#travel").append('<li>'+ data.response.results[i].sectionName +': ' + '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</li>')
-      console.log(data);
+  });
+
+$.getJSON("http://content.guardianapis.com/search?section=travel&api-key=test", function(data){
+    for (i=0; i<10 ;i++) {
+  $("#travel").append('<ol>'+ '<a href=' + data.response.results[i].webUrl + '>' + data.response.results[i].webTitle + '</a>' + '</ol>')
+    console.log(data);
     }
-    });
+  });
+
 });
