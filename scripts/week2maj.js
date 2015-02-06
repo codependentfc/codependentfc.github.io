3765db0a857b4082902453110dc29f346527f8a0
Jason Luu <jasoncluu@googlemail.com>
2015-02-06 15:00:52 +0000

    open links in new tabs

--- scripts/week2maj.js
+++ scripts/week2maj.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
   $.getJSON("http://content.guardianapis.com/search?show-fields=trailText&q=section=uk-news&api-key=test", function(data){
     for (i=0; i<10 ;i++) {
-  $("#uk-news").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '"">' + data.response.results[i].webTitle + '</a>' + '</li>')
+  $("#uk-news").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '" target="myiframe">' + data.response.results[i].webTitle + '</a>' + '</li>')
     console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?show-fields=trailText&q=section=sport&api-key=test", function(data){
     for (i=0; i<10 ;i++) {
-  $("#sport").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '"">' + data.response.results[i].webTitle + '</a>' + '</li>')
+  $("#sport").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '" target="myiframe">' + data.response.results[i].webTitle + '</a>' + '</li>')
     console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?show-fields=trailText&q=section=travel&api-key=test", function(data){
     for (i=0; i<10 ;i++) {
-  $("#travel").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '"">' + data.response.results[i].webTitle + '</a>' + '</li>')
+  $("#travel").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '"  target="myiframe">' + data.response.results[i].webTitle + '</a>' + '</li>')
     console.log(data);
     }
   });
