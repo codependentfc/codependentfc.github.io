f3a97428c38897c130c4eef7ff49a2975a61331a
Mark William Firth <markwilliamfirth@gmail.com>
2015-02-06 17:04:18 +0000

    Hash href for hand cursor

--- scripts/week2maj.js
+++ scripts/week2maj.js
@@ -9,21 +9,21 @@ $("#myiframe").attr("src", link);
   
   $.getJSON("http://content.guardianapis.com/search?section=uk-news&order-by=newest&show-fields=body%2CtrailText&page-size=10&api-key=yht9jzt3ccngxwgyknvfaj89", function(data){
     for (i=0; i<10 ;i++) {
-  $("#uk-news").append('<li><a class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
+  $("#uk-news").append('<li><a href="#" class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
     //console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?section=sport&order-by=newest&show-fields=body%2CtrailText&page-size=10&api-key=yht9jzt3ccngxwgyknvfaj89", function(data){
     for (i=0; i<10 ;i++) {
-  $("#sport").append('<li><a class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
+  $("#sport").append('<li><a href="#" class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
     //console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?section=travel&order-by=newest&show-fields=body%2CtrailText&page-size=10&api-key=yht9jzt3ccngxwgyknvfaj89", function(data){
     for (i=0; i<10 ;i++) {
-  $("#travel").append('<li><a class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
+  $("#travel").append('<li><a href="#" class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
    // console.log(data);
     }
   });
