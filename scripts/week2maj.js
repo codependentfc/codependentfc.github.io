7e2303cb14ff0749449f36188e7bad6c1578f730
Jason Luu <jasoncluu@googlemail.com>
2015-02-06 16:59:40 +0000

    finally iframe

--- scripts/week2maj.js
+++ scripts/week2maj.js
@@ -1,22 +1,30 @@
 $(document).ready(function(){
+
+
+$(document).on("click", "a", function() {
+var link = $(this).attr("linkid");
+$("#myiframe").attr("src", link);
+});
+
+  
   $.getJSON("http://content.guardianapis.com/search?section=uk-news&order-by=newest&show-fields=body%2CtrailText&page-size=10&api-key=yht9jzt3ccngxwgyknvfaj89", function(data){
     for (i=0; i<10 ;i++) {
-  $("#uk-news").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '" target="myiframe">' + data.response.results[i].webTitle + '</a>' + '</li>')
-    console.log(data);
+  $("#uk-news").append('<li><a class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
+    //console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?section=sport&order-by=newest&show-fields=body%2CtrailText&page-size=10&api-key=yht9jzt3ccngxwgyknvfaj89", function(data){
     for (i=0; i<10 ;i++) {
-  $("#sport").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '" target="myiframe">' + data.response.results[i].webTitle + '</a>' + '</li>')
-    console.log(data);
+  $("#sport").append('<li><a class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
+    //console.log(data);
     }
   });
 
 $.getJSON("http://content.guardianapis.com/search?section=travel&order-by=newest&show-fields=body%2CtrailText&page-size=10&api-key=yht9jzt3ccngxwgyknvfaj89", function(data){
     for (i=0; i<10 ;i++) {
-  $("#travel").append('<li><a data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" href="' + data.response.results[i].webUrl + '"  target="myiframe">' + data.response.results[i].webTitle + '</a>' + '</li>')
-    console.log(data);
+  $("#travel").append('<li><a class="links" data-toggle="tooltip" title="' + data.response.results[i].fields.trailText + '" linkid="' + data.response.results[i].webUrl + '">' + data.response.results[i].webTitle + '</a>' + '</li>')
+   // console.log(data);
     }
   });
 
